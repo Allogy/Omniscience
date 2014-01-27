bd827522bacbfde0c8a9aa823947a927eff6502a
Richard Venable <richard@epicfox.com>
2014-01-27 10:52:40 -0600

    Added moving target key path methods: Observe a target keyPath combination, but may be called again with a different target, stopping the observation for the original call.

--- Omniscience/NSObject+Omniscience.h
+++ Omniscience/NSObject+Omniscience.h
@@ -28,6 +28,15 @@
 - (void)observeTargetKeyPath:(_OMNITargetKeyPath *)targetKeyPath shouldCallBlockInitially:(BOOL)shouldCallBlockInitially shouldCallBlockPriorToUpdates:(BOOL)shouldCallBlockPriorToUpdates usingBlock:(void (^)(OMNINotification *notification))block;
 - (void)stopObservingTargetKeyPath:(_OMNITargetKeyPath *)targetKeyPath;
 
+/*
+ Observe a target keyPath combination, but may be called again with a different target, stopping the observation for the original call.
+ 
+ Please use the OMNITargetKeyPath macro to create the first parameter.
+ Stops observing the keyPath for any target if it had already been observing it with a different block.
+ */
+- (void)observeMovingTargetKeyPath:(_OMNITargetKeyPath *)targetKeyPath shouldCallBlockInitially:(BOOL)shouldCallBlockInitially shouldCallBlockPriorToUpdates:(BOOL)shouldCallBlockPriorToUpdates usingBlock:(void (^)(OMNINotification *notification))block;
+- (void)stopObservingMovingTargetKeyPath:(_OMNITargetKeyPath *)targetKeyPath;
+
 // DEPRECATED: use methods above instead
 - (void)observeTarget:(id)target keyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options usingBlock:(void (^)(OMNINotification *notification))block DEPRECATED_ATTRIBUTE;
 - (void)stopObservingTarget:(id)target keyPath:(NSString *)keyPath DEPRECATED_ATTRIBUTE;
--- Omniscience/NSObject+Omniscience.m
+++ Omniscience/NSObject+Omniscience.m
@@ -42,6 +42,30 @@
 	[self stopObserving:targetKeyPath.target keyPath:targetKeyPath.keyPath];
 }
 
+- (void)observeMovingTargetKeyPath:(_OMNITargetKeyPath *)targetKeyPath options:(NSKeyValueObservingOptions)options usingBlock:(void (^)(OMNINotification *))block
+{
+	[self stopObservingMovingTargetKeyPath:targetKeyPath];
+	[self observeTarget:targetKeyPath.target keyPath:targetKeyPath.keyPath options:options block:^(MAKVONotification *notification) {
+		block(notification.omniscienceNotification);
+	}];
+}
+
+- (void)observeMovingTargetKeyPath:(_OMNITargetKeyPath *)targetKeyPath shouldCallBlockInitially:(BOOL)shouldCallBlockInitially shouldCallBlockPriorToUpdates:(BOOL)shouldCallBlockPriorToUpdates usingBlock:(void (^)(OMNINotification *))block
+{
+	NSKeyValueObservingOptions options = NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld;
+	if (shouldCallBlockInitially)
+		options |= NSKeyValueObservingOptionInitial;
+	if (shouldCallBlockPriorToUpdates)
+		options |= NSKeyValueObservingOptionPrior;
+
+	[self observeMovingTargetKeyPath:targetKeyPath options:options usingBlock:block];
+}
+
+- (void)stopObservingMovingTargetKeyPath:(_OMNITargetKeyPath *)targetKeyPath
+{
+	[self stopObserving:nil keyPath:targetKeyPath.keyPath];
+}
+
 - (void)observeTarget:(id)target keyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options usingBlock:(void (^)(OMNINotification *))block
 {
 	[self observeTarget:target keyPath:keyPath options:options block:^(MAKVONotification *notification) {
